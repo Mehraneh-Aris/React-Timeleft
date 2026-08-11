d5672a58746a9b5d2eb59c66e615b3b2a39d23ae
mehraneh <arisdev.mehraneh@gmail.com>
2026-08-11 18:44:58 +0330

    feat: add countdown timer logic

--- src/components/timeLeft.jsx
+++ src/components/timeLeft.jsx
@@ -1,6 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import formatTime from "../utils/FormatTime"
 
 const TimeLeft = () => {
+    const[timeLeft,setTimeLeft] = useState( 1* 60)
+    useEffect(()=>{
+        if(timeLeft <= 0) return
+
+        const timer = setInterval(()=>{
+            setTimeLeft (prev=>prev-1)
+        },1000)
+
+        return ()=>clearInterval(timer)
+
+    },[timeLeft])
+
     return (
         <>
 
