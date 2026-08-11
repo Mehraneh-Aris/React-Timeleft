dcf6d2cb39e07a07c35e5092348f39a69edf7e19
mehraneh <arisdev.mehraneh@gmail.com>
2026-08-11 21:05:02 +0330

    feat: display countdown and discount status

--- src/components/timeLeft.jsx
+++ src/components/timeLeft.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import formatTime from "../utils/FormatTime"
+import styles from "../styles/CountdownTimer.module.css"
 
 const TimeLeft = () => {
     const[timeLeft,setTimeLeft] = useState( 1* 60)
@@ -15,9 +16,18 @@ const TimeLeft = () => {
     },[timeLeft])
 
     return (
-        <>
+        <div className={styles.card}>
+            <h2 className={styles.title}>تخفیف های ویژه</h2>
+            {
+              timeLeft > 0 ?(
+                <p className={styles.timer}>زمان باقیمانده : <span className={styles.time}>{formatTime(timeLeft)}</span></p>
+              )  : (
+                <p className={styles.expired}>زمان به پایان رسید</p>
+              )
+            }
 
-        </>
+            <button className={styles.button} disabled={timeLeft <= 0} >دریافت تخفیف</button>
+        </div>
     );
 
 }
