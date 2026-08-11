33370eef9aae34a0296b12f7e5055a71f0cc4b80
mehraneh <arisdev.mehraneh@gmail.com>
2026-08-11 18:20:09 +0330

    Initialize project using Create React App

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import TimeLeft from "../components/timeLeft";
+
+const Home = () => {
+    return (
+
+            <TimeLeft/>
+       
+    );
+
+}
+
+export default Home;
